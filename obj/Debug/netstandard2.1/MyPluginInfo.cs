83f88992399f5b130920eeccde077f52bd148d86
MBCollector672 <epicperson456@gmail.com>
2026-04-08 15:15:35 -0400

    fix gitignore

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace TheUltimateNumber
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MBCollector672.TheUltimateNumber";
-        public const string PLUGIN_NAME = "TheUltimateNumber";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
